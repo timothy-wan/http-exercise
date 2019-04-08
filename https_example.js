e149b2523c1ed9b834e062e3e22381531bed6cfa
Tim <tw5033@hotmail.com>
2019-04-08 03:33:53 +0000

    step 2 completed

--- https_example.js
+++ https_example.js
@@ -1,4 +1,17 @@
 var https = require('https');
 
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
 
-console.log('I did it!');
+// called by https when the request is made.
+var callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
